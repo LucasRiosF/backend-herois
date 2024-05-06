0087d376dc28009a937d68b6080d2a1e47956fdf
Lucas Rios <lucas.rios@aluno.senai.br>
2024-05-06 15:12:13 -0300

    feat: criação das tabelas na pasta database

--- database/script.sql
+++ database/script.sql
@@ -5,6 +5,17 @@ CREATE DATABASE lucas;
 CREATE TABLE herois (
   id SERIAL PRIMARY KEY,
   nome VARCHAR(100) NOT NULL,
+  poder VARCHAR(100) NOT NULL,
   nivel INT NOT NULL,
   pontos_vida INT NOT NULL
 );
+
+CREATE TABLE batalhas (
+    id SERIAL PRIMARY KEY,
+    id_heroi1 INT NOT NULL,
+    id_heroi2 INT NOT NULL,
+    id_vencedor INT NOT NULL,
+    FOREIGN KEY (id_heroi1) REFERENCES herois(id),
+    FOREIGN KEY (id_heroi2) REFERENCES herois(id),
+    FOREIGN KEY (id_vencedor) REFERENCES herois(id),
+)
